d2b2bade58252c4239e2028dc05053a171e54a43
rezapb2001 <rezapb2001@gmail.com>
2022-07-17 19:51:02 +0700

    coba

--- core/test/json_reader.dart
+++ core/test/json_reader.dart
@@ -4,8 +4,8 @@ String readJson(String name) {
   var dir = Directory.current.path;
   if (dir.endsWith('/test')) {
     dir = dir.replaceAll('/test', '');
-  } else if (dir.endsWith('core')) {
+  } if (dir.endsWith('core')) {
     return File('$dir/test/$name').readAsStringSync();
   }
-  return File('$dir/test/$name').readAsStringSync();
+  return File('$dir/core/test/$name').readAsStringSync();
 }
