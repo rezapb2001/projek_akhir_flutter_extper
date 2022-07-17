a5e9d45886031e868e649a7eacb0484777873849
rezapb2001 <rezapb2001@gmail.com>
2022-07-17 13:56:30 +0700

    dummy data update

--- movie/test/dummy_data/dummy_objects.dart
+++ movie/test/dummy_data/dummy_objects.dart
@@ -1,11 +1,10 @@
 import 'package:core/core.dart';
 import 'package:movie/movie.dart';
-import 'package:tv_series/tv_series.dart';
 
 final testMovie = Movie(
   adult: false,
   backdropPath: '/muth4OYamXf41G2evdrLEg8d3om.jpg',
-  genreIds: [14, 28],
+  genreIds: const [14, 28],
   id: 557,
   originalTitle: 'Spider-Man',
   overview:
@@ -21,7 +20,7 @@ final testMovie = Movie(
 
 final testMovieList = [testMovie];
 
-final testMovieDetail = MovieDetail(
+const testMovieDetail = MovieDetail(
   adult: false,
   backdropPath: 'backdropPath',
   genres: [Genre(id: 1, name: 'Action')],
@@ -43,7 +42,7 @@ final testWatchlistMovie = Movie.watchlist(
   overview: 'overview',
 );
 
-final testMovieTable = MovieTable(
+const testMovieTable = MovieTable(
   id: 1,
   title: 'title',
   posterPath: 'posterPath',
@@ -55,55 +54,4 @@ final testMovieMap = {
   'overview': 'overview',
   'posterPath': 'posterPath',
   'title': 'title',
-};
-
-final testTvSeries = Tv(
-    backdropPath: '/suopoADq0k8YZr4dQXcU6pToj6s.jpg',
-    genreIds: [10765, 18, 10759, 9648],
-    id: 1399,
-    originalName: 'Game of Thrones',
-    overview:
-    "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north. Amidst the war, a neglected military order of misfits, the Night'styles Watch, is all that stands between the realms of men and icy horrors beyond.",
-    popularity: 369.594,
-    posterPath: '/u3bZgnGQ9T01sWNhyveQz0wH0Hl.jpg',
-    firstAirDate: '2011-04-17',
-    name: 'Game of Thrones',
-    voteAverage: 8.3,
-    voteCount: 11504
-);
-
-final testTvSeriesList = [testTvSeries];
-
-final testTvSeriesDetail = TvDetail(
-  backdropPath: 'backdropPath',
-  genres: [Genre(id: 1, name: 'Action')],
-  id: 1,
-  originalName: 'originalName',
-  overview: 'overview',
-  posterPath: 'posterPath',
-  firstAirDate: 'firstAirDate',
-  name: 'name',
-  voteAverage: 1,
-  voteCount: 1,
-);
-
-final testWatchlistTvSeries = Tv.watchlist(
-  id: 1,
-  name: 'name',
-  posterPath: 'posterPath',
-  overview: 'overview',
-);
-
-final testTvSeriesTable = TvTable(
-  id: 1,
-  name: 'name',
-  posterPath: 'posterPath',
-  overview: 'overview',
-);
-
-final testTvSeriesMap = {
-    'id': 1,
-    'overview': 'overview',
-    'posterPath': 'posterPath',
-    'name': 'name',
 };
--- tv_series/test/dummy_data/dummy_objects.dart
+++ tv_series/test/dummy_data/dummy_objects.dart
@@ -1,54 +1,5 @@
 import 'package:core/core.dart';
-import 'package:movie/movie.dart';
 import 'package:tv_series/tv_series.dart';
-
-final testMovie = Movie(
-  adult: false,
-  backdropPath: '/muth4OYamXf41G2evdrLEg8d3om.jpg',
-  genreIds: [14, 28],
-  id: 557,
-  originalTitle: 'Spider-Man',
-  overview:
-      'After being bitten by a genetically altered spider, nerdy high school student Peter Parker is endowed with amazing powers to become the Amazing superhero known as Spider-Man.',
-  popularity: 60.441,
-  posterPath: '/rweIrveL43TaxUN0akQEaAXL6x0.jpg',
-  releaseDate: '2002-05-01',
-  title: 'Spider-Man',
-  video: false,
-  voteAverage: 7.2,
-  voteCount: 13507,
-);
-
-final testMovieList = [testMovie];
-
-final testMovieDetail = MovieDetail(
-  adult: false,
-  backdropPath: 'backdropPath',
-  genres: [Genre(id: 1, name: 'Action')],
-  id: 1,
-  originalTitle: 'originalTitle',
-  overview: 'overview',
-  posterPath: 'posterPath',
-  releaseDate: 'releaseDate',
-  runtime: 120,
-  title: 'title',
-  voteAverage: 1,
-  voteCount: 1,
-);
-
-final testWatchlistMovie = Movie.watchlist(
-  id: 1,
-  title: 'title',
-  posterPath: 'posterPath',
-  overview: 'overview',
-);
-
-final testMovieTable = MovieTable(
-  id: 1,
-  title: 'title',
-  posterPath: 'posterPath',
-  overview: 'overview',
-);
 
 final testMovieMap = {
   'id': 1,
@@ -59,7 +10,7 @@ final testMovieMap = {
 
 final testTvSeries = Tv(
     backdropPath: '/suopoADq0k8YZr4dQXcU6pToj6s.jpg',
-    genreIds: [10765, 18, 10759, 9648],
+    genreIds: const [10765, 18, 10759, 9648],
     id: 1399,
     originalName: 'Game of Thrones',
     overview:
@@ -74,7 +25,7 @@ final testTvSeries = Tv(
 
 final testTvSeriesList = [testTvSeries];
 
-final testTvSeriesDetail = TvDetail(
+const testTvSeriesDetail = TvDetail(
   backdropPath: 'backdropPath',
   genres: [Genre(id: 1, name: 'Action')],
   id: 1,
@@ -94,7 +45,7 @@ final testWatchlistTvSeries = Tv.watchlist(
   overview: 'overview',
 );
 
-final testTvSeriesTable = TvTable(
+const testTvSeriesTable = TvTable(
   id: 1,
   name: 'name',
   posterPath: 'posterPath',
